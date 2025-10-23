beadb95df6245912b6d1bb6cc3b14cdb40cb5f98
Lorenzo Galassi <lorenzo.galassi91@gmail.com>
2025-10-23 23:23:46 +0200

    feat: Search button

--- app/components/HeaderComponent.vue
+++ app/components/HeaderComponent.vue
@@ -1,15 +1,34 @@
 <template>
-	<div class="w-full">
-		<h1 class="text-5xl font-semibold tracking-wide uppercase">{{ title }}</h1>
-	</div>
+  <div class="w-full flex justify-between items-center">
+    <h1 class="text-5xl font-semibold tracking-wide uppercase">{{ title }}</h1>
+
+    <div class="flex flex-row items-center justify-end">
+      <input
+        type="text"
+        placeholder="Search..."
+        class="-mr-8 px-2 py-1 h-10 border border-zinc-300 z-10 rounded-lg focus:outline-none focus:ring-2 focus:ring-amber-500/50 transition-all duration-300"
+				:class="showSearch ? 'w-full' : 'w-0'"
+      />
+      <IconComponent
+        ref="target"
+        icon="game-icons:crosshair"
+        :width="48"
+        :height="48"
+        class="hover:cursor-pointer z-20 transition-all duration-300 hover:rotate-90 text-amber-600 bg-zinc-300 rounded-full"
+				@click="showSearch = !showSearch"
+      />
+    </div>
+  </div>
 </template>
 
 <script setup>
-import { CONSTANTS, classes } from '#imports';
+  import { CONSTANTS } from "#imports";
+  import IconComponent from "./IconComponent.vue";
 
-const title = ref()
+  const title = ref();
+	const showSearch = ref(false);
 
-onMounted(() => {
-	title.value = CONSTANTS.TITLE;
-});
+  onMounted(() => {
+    title.value = CONSTANTS.TITLE;
+  });
 </script>
